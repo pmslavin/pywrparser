0a23c4eba7ea52b69156cd9ea83a206cafc14531
Paul Slavin <pmslavin@gmail.com>
2022-04-28 23:37:19 +0100

    Experimental rulesets

---
+++ pywrparser/rulesets/test.py
@@ -0,0 +1,20 @@
+from pywrparser.types.node import PywrNode
+from pywrparser.types.parameter import PywrParameter
+from pywrparser.utils import match
+
+
+class AltNode(PywrNode):
+    def __init__(self, data):
+        super().__init__(data)
+
+    def rule_no_xstart(self):
+        if self.name:
+            assert not self.name.lower().startswith('x'), "Node name may not begin with letter 'x'"
+
+
+class AltParameter(PywrParameter):
+    def __init__(self, name, data):
+        super().__init__(name, data)
+
+    def rule_no_ystart(self):
+        assert not self.name.lower().startswith('y'), "Parameter name may not begin with letter 'y'"
--- pywrparser/types/__init__.py
+++ pywrparser/types/__init__.py
@@ -2,7 +2,9 @@ from .timestepper import PywrTimestepper
 from .metadata import PywrMetadata
 from .scenario import PywrScenario
 from .table import PywrTable
-from .parameter import PywrParameter
+#from .parameter import PywrParameter
 from .recorder import PywrRecorder
-from .node import PywrNode
+#from .node import PywrNode
+from pywrparser.rulesets.test import AltNode as PywrNode
+from pywrparser.rulesets.test import AltParameter as PywrParameter
 from .edge import PywrEdge
